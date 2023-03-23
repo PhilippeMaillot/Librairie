0b428555ec9baea86952a79605ae52f6fbd3162e
Philippe Maillot <maillotphilippe78@gmail.com>
2023-03-23 18:02:44 +0100

    insc

--- inscription.php
+++ inscription.php
@@ -1,11 +1,11 @@
 <?php include('nav.php') ?>
-            <div class="cadre2">
-        <form action="php_insc.php" method="get">
-            <p>Nom <input name="nom" type="text"/></p> 
-            <p>Email <input name="email" type="email"/></p>
-            <p>Mot de passe <input name="mdp" type="password"></p>
-        <input type="submit" value="Confirmer"/><br/>
+<div class="cadre2">
+    <form action="php_insc.php" method="get">
+        <p>Nom <input id="nom" name="nom" type="text"/></p> 
+        <p>Email <input id="email" name="email" type="email"/></p>
+        <p>Mot de passe <input id="mdp" name="mdp" type="password"></p>
+        <input type="submit" onclick="return validerFormulaire()" class="btn2" value="Confirmer"/><br/>
     </form> 
-    </div>
+</div>
 </body>
 </html>
