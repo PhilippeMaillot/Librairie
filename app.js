f18aa26703a29899541d4988d51d7da87307002e
Philippe Maillot <maillotphilippe78@gmail.com>
2023-03-23 18:05:51 +0100

    insc

--- app.js
+++ app.js
@@ -16,16 +16,3 @@ for (var i = 0; i < links.length; i++) {
   maxHeight += links[i].offsetHeight;
 }
 content.style.maxHeight = maxHeight + "px";
-
-function validerFormulaire() {
-  var nom = document.getElementById("nom").value;
-  var email = document.getElementById("email").value;
-  var mdp = document.getElementById("mdp").value;
-  
-  if (nom == "" || email == "" || mdp == "") {
-    alert("Veuillez remplir tous les champs");
-    return false; // empêche l'envoi du formulaire si des champs sont vides
-  }
-  
-  return true; // autorise l'envoi du formulaire si tous les champs sont remplis
-}
--- inscription.php
+++ inscription.php
@@ -1,4 +1,18 @@
 <?php include('nav.php') ?>
+<script>
+function validerFormulaire() {
+  var nom = document.getElementById("nom").value;
+  var email = document.getElementById("email").value;
+  var mdp = document.getElementById("mdp").value;
+  
+  if (nom == "" || email == "" || mdp == "") {
+    alert("Veuillez remplir tous les champs");
+    return false; // empêche l'envoi du formulaire si des champs sont vides
+  }
+  
+  return true; // autorise l'envoi du formulaire si tous les champs sont remplis
+}
+</script>
 <div class="cadre2">
     <form action="php_insc.php" method="get">
         <p>Nom <input id="nom" name="nom" type="text"/></p> 
